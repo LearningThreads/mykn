5599e64cdd3eb768d86b3ab2126cd2353f82953a
Trevor Gile <sleuthg@gmail.com>
2016-05-10 07:08:07 -0700

    added capability to export data and included permission to access Google Analytics page

--- js/options.js
+++ js/options.js
@@ -5,50 +5,28 @@ function clearAll() {
     graphs:'[]'
   };
   chrome.storage.local.set({"ltdb":db,"currentThreadName": ""}, function() {
-    //console.log('Cleared');
-    //chrome.storage.local.get("ltdb", function(obj) {
-    //  console.log(obj);
-    //})
   });
 }
 
+// Export the network to a file.  Adopted from Rob W. StackOverflow answer:
+// http://stackoverflow.com/questions/23160600/chrome-extension-local-storage-how-to-export
+function exportNetwork() {
+  chrome.storage.local.get(null, function(obj) {
+    // Convert to a string
+    var str = JSON.stringify(obj);
+    var url = 'data:application/json;base64,' + btoa(str);
+    chrome.downloads.download({
+      url: url,
+      filename: 'mykn_network.json',
+      saveAs: true
+    });
+  });
+}
+
+// Add listeners
 document.getElementById('clearAll').addEventListener('click', clearAll);
+document.getElementById('exportNetwork').addEventListener('click', exportNetwork);
 
-
-// ***** FROM CHROME API EXAMPLES ***** //
-
-//// Saves options to chrome.storage
-//function save_options() {
-//  var color = document.getElementById('color').value;
-//  var likesColor = document.getElementById('like').checked;
-//  chrome.storage.sync.set({
-//    favoriteColor: color,
-//    likesColor: likesColor
-//  }, function() {
-//    // Update status to let user know options were saved.
-//    var status = document.getElementById('status');
-//    status.textContent = 'Options saved.';
-//    setTimeout(function() {
-//      status.textContent = '';
-//    }, 750);
-//  });
-//}
-//
-//// Restores select box and checkbox state using the preferences
-//// stored in chrome.storage.
-//function restore_options() {
-//  // Use default value color = 'red' and likesColor = true.
-//  chrome.storage.sync.get({
-//    favoriteColor: 'red',
-//    likesColor: true
-//  }, function(items) {
-//    document.getElementById('color').value = items.favoriteColor;
-//    document.getElementById('like').checked = items.likesColor;
-//  });
-//}
-//document.addEventListener('DOMContentLoaded', restore_options);
-//document.getElementById('save').addEventListener('click',
-//  save_options);
 
 
 
