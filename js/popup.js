dfd6bc1d36d8ae2f57a850344a98442b5b11f716
Trevor Gile <sleuthg@gmail.com>
2016-05-10 08:51:13 -0700

    implemented opt-out for analytics tracking, which is required by the Google User Privacy policy

--- js/popup.js
+++ js/popup.js
@@ -1,22 +1,34 @@
-// You'll usually only ever have to create one service instance.
-var service = analytics.getService('mykn');
 
-// Ask the user if they would like to opt-out
-service.getConfig().addCallback(
-  /** @param {!analytics.Config} config */
-  function(config) {
-    //var permitted = myApp.askUser('Allow anonymous usage tracking?');
-    var permitted = true;
-    config.setTrackingPermitted(permitted);
-    // If "permitted" is false the library will automatically stop
-    // sending information to Google Analytics and will persist this
-    // behavior automatically.
-  });
+var service, tracker;
 
-// You can create as many trackers as you want. Each tracker has its own state
-// independent of other tracker instances.
-var tracker = service.getTracker('UA-70594003-2');  // Supply your GA Tracking ID.
+function mykn_initialize() {
+  // You'll usually only ever have to create one service instance.
+  var service = analytics.getService('mykn');
 
-// Report when the popup is loaded
-tracker.sendAppView('Loaded');
+  // Ask the user if they would like to opt-out
+  service.getConfig().addCallback(mykn_analytics_config);
+
+  // You can create as many trackers as you want. Each tracker has its own state
+  // independent of other tracker instances.
+  tracker = service.getTracker('UA-70594003-2');  // Supply your GA Tracking ID.
+
+  // Report when the popup is loaded
+  tracker.sendAppView('Loaded');
+}
+
+function mykn_analytics_config(config) {
+
+  // Grab the checkbox
+  var checkboxTracking = document.getElementById('analyticsPermission');
+  checkboxTracking.checked = config.isTrackingPermitted();
+
+  // When the checkbox is changed, set the analytics tracking permission
+  checkboxTracking.onchange = function() {
+    config.setTrackingPermitted(checkboxTracking.checked);
+  };
+}
+
+window.onload = mykn_initialize;
+
+
 
